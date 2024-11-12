bcd2546cc5bab29bf55efe47c90c98b507efcc2b
ben <bwestcott@icloud.com>
2024-11-12 01:10:44 -0500

    new engine work

--- include/engine.h
+++ include/engine.h
@@ -11,11 +11,6 @@
 #include <concepts>
 #include <algorithm>
 
-template<typename T>
-using rect_init_list = std::initializer_list<std::initializer_list<T>>;
-
-template<typename T>
-using init_list = std::initializer_list<T>;
 
 struct matrix_orientation
 {
@@ -77,6 +72,11 @@ struct static_value_caster
  */
 struct engine_helper
 {
+    template<typename T>
+    using rect_init_list = std::initializer_list<std::initializer_list<T>>;
+
+    template<typename T>
+    using init_list = std::initializer_list<T>;
 
     template<typename X, typename Y>
     static constexpr bool sizes_equal(X x, Y y)
@@ -112,7 +112,7 @@ struct engine_helper
     static constexpr void validate_length(X x)
     {
         //return x >= static_cast<X>(0);
-        if(x >= static_cast<X>(0))
+        if(x < static_cast<X>(0))
         {
             throw std::runtime_error("invalid length.");
         }
@@ -128,12 +128,12 @@ struct engine_helper
     }
 
     template<typename X>
-    static constexpr void validate_room(X x)
+    static constexpr void validate_reach(X x)
     {
         //return x > static_cast<X>(0);
-        if(x > static_cast<X>(0))
+        if(x < static_cast<X>(1))
         {
-            throw std::runtime_error("invalid room.");
+            throw std::runtime_error("invalid reach.");
         }
     }
 
@@ -150,9 +150,8 @@ struct engine_helper
         }
     }
 
-
     template<typename T>
-    static constexpr void swap(T t1, T t2) noexcept
+    static constexpr void swap(T & t1, T & t2) noexcept
     {
         if(&t1 != &t2)
         {
@@ -163,10 +162,20 @@ struct engine_helper
     }
 
     template<class Egn, typename R, typename C>
-    static constexpr void validate(Egn & eng, R nbr_rows, C nbr_cols);
+    static constexpr void validate(Egn & eng, R nbr_rows, C nbr_cols)
+    {
+        auto nrows = static_cast<typename Egn::index_type>(nbr_rows);
+        auto ncols = static_cast<typename Egn::index_type>(nbr_cols);
 
-    template<class Egn, typename X>
-    static constexpr void validate(Egn & eng, X size);
+        if(nrows != eng.rows() || ncols != eng.cols())
+        {
+            eng.reshape(nrows, eng.row_reach(), ncols, eng.col_reach());
+        }
+
+    }
+
+    //template<class Egn, typename X>
+    //static constexpr void validate(Egn & eng, X size);
 
     template<class EgnX, class EgnY>
     static constexpr void copy2(EgnX const& src, EgnY & dst)
@@ -183,14 +192,16 @@ struct engine_helper
         src_index_type nbr_src_rows = src.rows();
         src_index_type nbr_src_cols = src.cols();
         
-        // TODO: validate
+        validate(dst, nbr_src_rows, nbr_src_cols);
     
         src_index_type src_idx, src_jdx;
         dst_index_type dst_idx, dst_jdx;
 
-        for(src_idx = dst_idx = 0; src_idx < nbr_src_rows; src_idx++, dst_idx++)
+        src_idx = dst_idx = 0;
+        for(; src_idx < nbr_src_rows; src_idx++, dst_idx++)
         {
-            for(src_jdx = dst_jdx = 0; src_jdx < nbr_src_cols; src_jdx++, dst_jdx++)
+            src_jdx = dst_jdx = 0;
+            for(; src_jdx < nbr_src_cols; src_jdx++, dst_jdx++)
             {
                 dst(dst_idx, dst_jdx) = static_cast<dst_data_type>(src(src_idx, src_jdx));
             }
@@ -204,28 +215,36 @@ struct engine_helper
     template<typename T, typename EgnY>
     static constexpr void copy2(rect_init_list<T> src, EgnY & dst)
     requires
-        std::convertible_to<T, typename Egn::data_type>
+        std::convertible_to<T, typename EgnY::data_type>
         // should also require that Egn be atleast writable
     {
-        using index_type = typename Egn::index_type;
+        using index_type = typename EgnY::index_type;
+        using data_type = typename EgnY::data_type;
 
         validate_init_list(src);
 
         auto nbr_rows = src.size();
         auto nbr_cols = src.begin()->size();
 
-        // TODO: validate 
+        validate(dst, nbr_rows, nbr_cols);
 
-        for(auto r : src)
+        index_type dst_idx, dst_jdx;
+        auto src_idx = src.begin();
+
+        for(; dst_idx < dst.rows(); dst_idx++, src_idx++)
         {
-            
+            auto src_jdx = src_idx->begin();
+            for(dst_jdx = 0; dst_jdx < dst.cols(); dst_jdx++, src_jdx++)
+            {
+                dst(dst_idx, dst_jdx) = static_cast<data_type>(*src_jdx);
+            }
         }
-
-
-
-
     }
-    // TODO: mdspan copy2
+    
+    //static constexpr void copy2(Egn & dst, mdspan<T, extents<IT, X0, X1>, SL, SA> const& src)
+    //static constexpr void copy2(Egn & dst, Ctnr const& src)
+    //static constexpr void copy2(Egn & dst, mdspan<T, extents<IT, X0>, SL, SA> const& src)
+    //static constexpr void copy2(Egn & dst, initializer_list<T> src)
     
     template<class Egn, typename R1, typename R2, typename C1, typename C2>
     static constexpr void move_data(Egn & src, Egn & dst, R1 ilb_inc, R2 iub_ex, C1 jlb_inc, C2 jub_ex)
@@ -248,9 +267,9 @@ struct engine_helper
     }
     
     template<class Egn, typename R, typename C>
-    static constexpr void move_elements(Egn & src, Egn & dst, R nbr_rows, C nbr_cols)
+    static constexpr void move_data(Egn & src, Egn & dst, R nbr_rows, C nbr_cols)
     {
-        move_elements<Egn, Egn, R, R, C, C>(src, dst, 0, nbr_rows, 0, nbr_cols);
+        move_data<Egn, R, R, C, C>(src, dst, 0, nbr_rows, 0, nbr_cols);
     }
 
     template<class Egn, typename C1, typename C2, typename T>
@@ -299,7 +318,50 @@ struct engine_helper
         }
     }
 
+    template<class EgnX, class EgnY>
+    static constexpr bool compare_exact(EgnX const& lhs, EgnY const& rhs)
+    {
+        using index_type_lhs = typename EgnX::index_type;
+        using index_type_rhs = typename EgnY::index_type;
 
+        index_type_lhs lhs_nbr_rows = lhs.rows();
+        index_type_lhs lhs_nbr_cols = lhs.cols();
+        
+        index_type_rhs rhs_nbr_rows = rhs.rows();
+        index_type_rhs rhs_nbr_cols = rhs.cols();
+
+        if(!sizes_equal(lhs_nbr_rows, lhs_nbr_cols, rhs_nbr_rows, rhs_nbr_cols))
+        {
+            return false;
+        }
+
+        index_type_lhs lhs_i, lhs_j;
+        index_type_rhs rhs_i, rhs_j;
+
+        lhs_i = static_cast<index_type_lhs>(0);
+        rhs_i = static_cast<index_type_rhs>(0);
+
+        for(; lhs_i < lhs_nbr_rows; lhs_i++, rhs_i++)
+        {
+            lhs_j = static_cast<index_type_lhs>(0);
+            rhs_j = static_cast<index_type_rhs>(0);
+
+            for(; lhs_j < lhs_nbr_cols; lhs_j++, rhs_j++)
+            {
+                if(lhs(lhs_i, lhs_j) !=  rhs(rhs_i, rhs_j))
+                {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    //static constexpr bool compare_exact(Egn const& lhs, rect_init_list<T> rhs);
+    //static constexpr bool compare_exact(Egn const& lhs, mdspan<T, extents<IT, X0, X1>, SL, SA> const& src)
+    //static constexpr bool compare_exact(Egn & dst, mdspan<T, extents<IT, X0>, SL, SA> const& src)
+    //static constexpr bool compare_exact(Egn & dst, initializer_list<T> src)
+    //static constexpt bool compare_exact(Egn & dst, Ctnr const& src)
 };
 
 
@@ -327,7 +389,7 @@ struct matrix_data
     using index_type = std::size_t;
 
     using extents_type = std::extents<index_type, std::dynamic_extent, std::dynamic_extent>;
-    using stride_type = std::layout_stride;
+    using stride_type = std::layout_left;
 
     using mdspan_type = typename std::mdspan<T, extents_type, stride_type>;
     using const_mdspan_type = typename std::mdspan<T const, extents_type, stride_type>;
@@ -374,6 +436,13 @@ struct matrix_storage_engine
     using storage_type = matrix_data<T, Alloc, L>;
     using self_type = matrix_storage_engine;
     using orient = matrix_orientation;
+    using helper = engine_helper;
+
+    template<typename U>
+    using rect_init_list = std::initializer_list<std::initializer_list<U>>;
+
+    template<typename U>
+    using init_list = std::initializer_list<U>;
 
 public:
 
@@ -400,28 +469,62 @@ public:
     constexpr matrix_storage_engine & operator=(matrix_storage_engine const& other) = default;
     constexpr matrix_storage_engine & operator=(matrix_storage_engine && other) = default;
 
-    constexpr matrix_storage_engine(index_type nbr_rows, index_type nbr_cols);
-    constexpr matrix_storage_engine(index_type nbr_rows, index_type nbr_cols, index_type row_reach, index_type col_reach);
+    constexpr matrix_storage_engine(index_type nbr_rows, index_type nbr_cols)
+    : m_data()
+    {
+        __reshape(nbr_rows, nbr_rows, nbr_cols, nbr_cols);
+    }
+    constexpr matrix_storage_engine(index_type nbr_rows, index_type nbr_cols, index_type row_reach, index_type col_reach)
+    : m_data()
+    {
+        __reshape(nbr_rows, row_reach, nbr_cols, col_reach);
+    }
     
     //constexpr matrix_storage_engine(index_type nbr_rows);
     //constexpr matrix_storage_engine(index_type nbr_rows, index_type row_reach);
     //constexpr matrix_storage_engine(index_type nbr_cols);
     //constexpr matrix_storage_engine(index_type nbr_cols, index_type col_reach);
 
-    constexpr matrix_storage_engine(index_type size);
+    //constexpr matrix_storage_engine(index_type size);
     //constexpr matrix_storage_engine(index_type size, index_type room);
 
-    template<typename R>
-    constexpr matrix_storage_engine(rect_init_list<R> lst);
-
-    template<typename R>
-    constexpr matrix_storage_engine & operator=(rect_init_list<R> lst);
+    template<typename Egn>
+    constexpr matrix_storage_engine(Egn const& other)
+    requires
+        std::convertible_to<typename Egn::data_type, data_type>
+    : m_data()
+    {
+        helper::copy2(other, *this);
+    }
 
     template<typename Egn>
-    constexpr matrix_storage_engine(Egn const& other);
+    constexpr matrix_storage_engine & operator=(Egn const& other)
+    requires
+        std::convertible_to<typename Egn::data_type, data_type>
+    {
+        helper::copy2(other, *this);
+        return *this;
+    }
 
-    template<typename Egn>
-    constexpr matrix_storage_engine & operator=(Egn const& other);
+    template<typename R>
+    constexpr matrix_storage_engine(rect_init_list<R> lst)
+    requires
+        std::convertible_to<R, data_type>
+    : m_data()
+    {
+        helper::copy2(lst, *this);
+    }
+
+    template<typename R>
+    constexpr matrix_storage_engine & operator=(rect_init_list<R> lst)
+    requires
+        std::convertible_to<R, data_type>
+    {
+        helper::copy2(lst, *this);
+        return *this;
+    }
+
+
 
     //template<typename R, typename IT, size_t X, size_t Y, typename LP, typename AP>
     //constexpr matrix_storage_engine(std::mdspan<R, std::extents<IT, X, Y>, LP, AP> const& other);
@@ -461,7 +564,7 @@ public:
         return m_data.m_rows * m_data.m_cols;
     }
     
-    constexpr index_type room(void) const noexcept
+    constexpr index_type reach(void) const noexcept
     {
         return m_data.m_row_reach * m_data.m_col_reach;
     }
@@ -499,17 +602,143 @@ public:
     {
         return m_data.m_data[orient::offset(j, i, m_data.m_row_reach)];
     }
+
+    //constexpr mdspan_type span() noexcept
+    //constexpr const_mdspan_type span() const noexcept
     
+    constexpr void reshape(index_type nbr_rows, index_type row_reach, index_type nbr_cols, index_type col_reach)
+    {
+        __reshape(nbr_rows, row_reach, nbr_cols, col_reach);
+    }
+
+    constexpr void reshape_cols(index_type nbr_cols, index_type col_reach)
+    {
+        __reshape(m_data.m_rows, m_data.row_reach, nbr_cols, col_reach);
+    }
+
+    constexpr void reshape_rows(index_type nbr_rows, index_type row_reach)
+    {
+        __reshape(nbr_rows, row_reach, m_data.m_rows, m_data.m_row_reach);
+    }
+
+    constexpr void swap(self_type & other) noexcept
+    {
+        helper::swap(m_data, other.m_data);
+    }
+
     
 private:
 
-    constexpr void reshape(index_type nbr_rows, index_type row_reach, index_type nbr_cols, index_type col_reach);
-    constexpr void reshape_rows(index_type nbr_rows, index_type row_reach);
-    constexpr void reshape_cols(index_type nbr_cols, index_type col_reach);
+    constexpr void __reshape(index_type nbr_rows, index_type row_reach, index_type nbr_cols, index_type col_reach)
+    {
+        helper::validate_length(nbr_rows);
+        helper::validate_length(nbr_cols);
+        helper::validate_reach(row_reach);
+        helper::validate_reach(col_reach);
+
+        //std::cout << "here" << "\n";
+        
+        if(nbr_rows > m_data.m_row_reach || row_reach != m_data.m_row_reach ||
+           nbr_cols > m_data.m_col_reach || col_reach != m_data.m_col_reach)
+        {
+            // ensure that the rowreach is >= nbr_rows, and colreach >= nbr_cols
+            row_reach = std::max(nbr_rows, row_reach);
+            col_reach = std::max(nbr_cols, col_reach);
+
+            //std::cout << "here again" << nbr_rows << "\t" << nbr_cols << "\n";
+
+            self_type to;
+            to.m_data.m_data.resize(row_reach * col_reach);
+            to.m_data.m_rows = nbr_rows;
+            to.m_data.m_cols = nbr_cols;
+            to.m_data.m_row_reach = row_reach;
+            to.m_data.m_col_reach = col_reach;
+
+            //std::cout << "here again" << to.m_data.m_rows << "\t" << to.m_data.m_cols << "\n";
+
+            helper::move_data
+            (
+                *this,
+                to,
+                std::min(nbr_rows, m_data.m_rows),
+                std::min(nbr_cols, m_data.m_cols)
+            );
+
+            helper::swap(m_data, to.m_data);
+
+        }
+        else
+        {
+            if(nbr_rows < m_data.m_rows)
+            {
+                helper::fill_rows(*this, nbr_rows, m_data.m_rows, T{});
+                m_data.m_rows = nbr_rows;
+            }
+            if(nbr_cols < m_data.m_cols)
+            {
+                helper::fill_cols(*this, nbr_cols, m_data.m_cols, T{});
+                m_data.m_cols = nbr_cols;
+            }
+        }
+
+    }
+
+    constexpr void __reshape_rows(index_type nbr_rows, index_type row_reach)
+    {
+        helper::validate_length(nbr_rows);
+        helper::validate_reach(row_reach);
+
+        if(nbr_rows > m_data.m_row_reach || row_reach != m_data.m_row_reach)
+        {
+            row_reach = std::max(nbr_rows, row_reach);
+
+            self_type to;
+            to.m_data.m_data.resize(row_reach * m_data.m_col_reach);
+            to.m_data.m_rows = nbr_rows;
+            to.m_data.m_row_reach = row_reach;
+
+            helper::move_data(*this, to, std::min(nbr_rows, m_data.m_rows), m_data.m_cols);
+            helper::swap(m_data, to.m_data);
+        }
+        else
+        {
+            if(nbr_rows < m_data.m_rows)
+            {
+                helper::fill_rows(*this, nbr_rows, m_data.m_rows, T{});
+                m_data.m_rows = nbr_rows;
+            }
+        }
+    }
     
+    constexpr void __reshape_cols(index_type nbr_cols, index_type col_reach)
+    {
+        helper::validate_length(nbr_cols);
+        helper::validate_reach(col_reach);
 
+        if(nbr_cols > m_data.m_col_reach || col_reach != m_data.m_col_reach)
+        {
+            col_reach = std::max(nbr_cols, col_reach);
 
+            self_type to;
+            to.m_data.m_data.resize(col_reach * m_data.m_row_reach);
+            to.m_data.m_cols = nbr_cols;
+            to.m_data.m_col_reach = col_reach;
 
+            helper::move_data(*this, to, m_data.m_rows, std::min(nbr_cols, m_data.m_cols));
+            helper::swap(m_data, to.m_data);
+        }
+        else
+        {
+            if(nbr_cols < m_data.m_cols)
+            {
+                helper::fill_cols(*this, nbr_cols, m_data.m_cols, T{});
+                m_data.m_cols = nbr_cols;
+            }
+        }
+    }
+
+    //template<typename ST?, typename MSE>
+    //static constexpr ST make_mdspan(MSE & mse) noexcept
 };
 
 
---
+++ tests/test_engine.cpp
@@ -0,0 +1,12 @@
+//
+//  test_engine.cpp
+//  Created by Ben Westcott on 9/8/24.
+//
+
+/*
+ * Tests for first part of engine support
+ * they dont rely on the matrix_data or matrix_storage_engine classes
+ * and should be tested before them
+ */
+
+ 
